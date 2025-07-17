d16b4f898130fc12113113be733303703b9817de
ZYAD NASR <156793692+zyadnasr@users.noreply.github.com>
2025-07-17 04:49:58 +0300

    Add capacitor.config.ts 1752716995820

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.hassan.coffee',
+  appName: 'Hassan Coffee',
+  webDir: 'out'
+};
+
+export default config;
